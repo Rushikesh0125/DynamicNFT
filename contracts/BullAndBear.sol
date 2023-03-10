b6450a9e291d3f961054cb655d030efa221013f8
Rushikesh Jadhav <hellorushihere@gmail.com>
2023-03-10 19:36:14 +0530

    upkeep

--- contracts/BullAndBear.sol
+++ contracts/BullAndBear.sol
@@ -5,19 +5,89 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
 
-contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+
+contract BullAndBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, KeeperCompatible {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    uint public interval;
+    uint public lastTimeStamp;
 
-    constructor() ERC721("BullAndBear", "BAB") {}
+    AggregatorV3Interface public priceFeed;
+    int256 currentPrice;
+
+    constructor(uint256 updatedInterval, address _priceFeed) ERC721("BullAndBear", "BAB") {
+        interval = updatedInterval;
+        lastTimeStamp = block.timestamp;
+        priceFeed = AggregatorV3Interface(_priceFeed);
+        currentPrice = getLatestPrice(); 
+    }
+
+    string [] BullUris = [
+        "https://ipfs.io/ipfs/QmS1v9jRYvgikKQD6RrssSKiBTBH3szDK6wzRWF4QBvunR?filename=gamer_bull.json",
+        "https://ipfs.io/ipfs/QmRsTqwTXXkV8rFAT4XsNPDkdZs5WxUx9E5KwFaVfYWjMv?filename=party_bull.json",
+        "https://ipfs.io/ipfs/Qmc3ueexsATjqwpSVJNxmdf2hStWuhSByHtHK5fyJ3R2xb?filename=simple_bull.json"
+    ];
+
+    string [] BearUris = [
+        "https://ipfs.io/ipfs/QmQMqVUHjCAxeFNE9eUxf89H1b7LpdzhvQZ8TXnj4FPuX1?filename=beanie_bear.json",
+        "https://ipfs.io/ipfs/QmP2v34MVdoxLSFj1LbGW261fvLcoAsnJWHaBK238hWnHJ?filename=coolio_bear.json",
+        "https://ipfs.io/ipfs/QmZVfjuDiUfvxPM7qAvq8Umk3eHyVh7YTbFon973srwFMD?filename=simple_bear.json"
+    ];
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        string defaultURI = BearUris[0];
+        _setTokenURI(tokenId, defaultURI);
+    }
+
+    function checkUpKeep(bytes calldata) external view override returns(bool upKeepNeeded, bytes memory){
+        upKeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+    }
+
+    function performUpKeep(bytes calldata) external override{
+        if((block.timestamp - lastTimeStamp) > interval){
+            lastTimeStamp = block.timestamp;
+            int256 latestPrice = getLatestPrice();
+            if(latestPrice == currentPrice){
+                return;
+            }
+            if(latestPrice < currentPrice){
+                updateAllTokenUri("bear");
+            }else{
+                updateAllTokenUri("bull");
+            }
+
+            currentPrice = latestPrice;
+
+        }
+    }
+
+    function getLatestPrice() public view returns(int256){
+        (int256 price) = priceFeed.latestRoundData();
+
+        return price;
+    }
+
+    function updateAllTokenUri(string memory trend) internal{
+        if(compareStrings(trend, "bull")){
+            for(uint i = 0; i < _tokenIdCounter.current(); i++){
+                _setTokenURI(i, BullUris[0]);
+            }
+        }else{
+            for(uint i = 0; i < _tokenIdCounter.current(); i++){
+                _setTokenURI(i, BearUris[0]);
+            }
+        }
+    }
+
+    function compareStrings(string memory a, string memory b) internal returns(bool){
+        return (keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b)));
     }
 
     // The following functions are overrides required by Solidity.
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,25 +7,14 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const BAB = await hre.ethers.getContractFactory("BullAndBear");
+  const bab = await BAB.deploy();
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await bab.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`BAB deployed to ${lock.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
